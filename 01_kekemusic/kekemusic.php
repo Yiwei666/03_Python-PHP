9648a243f88c16b11b747d8e52ccdbd37b578f2f
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-06-21 20:30:55 +0800

    Update kekemusic.php

--- 01_kekemusic/kekemusic.php
+++ 01_kekemusic/kekemusic.php
@@ -1,7 +1,13 @@
+<?php
+$logoUrl = "http://domain.com/00_logo/kekemusic.png";
+$musicFile = '/home/01_html/04_kekemusic/finalmusic.txt';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="<?php echo $logoUrl; ?>">
     <title>顺序播放音频</title>
     <style>
         /* 容器居中对齐 */
@@ -15,7 +21,7 @@
             display: flex;
             flex-direction: column;
             align-items: flex-start;
-            width: 600px;
+            width: 400px;
         }
     </style>
 </head>
@@ -24,15 +30,20 @@
     <div class="container">
         <div class="audio-player">
             <?php
-            // 定义文件路径变量
-            $filePath = '/home/01_html/04_kekemusic/finalmusic.txt';
             // 读取文件内容，每个链接占据一行
-            $links = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            $links = file($musicFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
             // 逐个播放链接音频
             foreach ($links as $index => $link) {
-                echo "<p>正在播放 $link...</p>";
-                echo "<audio controls onended=\"playNext($index)\"><source src=\"$link\" type=\"audio/mpeg\"></audio>";
+                // 拆分日期和链接
+                $parts = explode(',', $link);
+                $date = $parts[0];
+                $url = $parts[1];
+
+                // 输出日期和链接
+                // echo "<p><span>$date</span><span>：正在播放 $url...</span></p>";
+		echo "<p><span>$date</span></p>";
+                echo "<audio controls onended=\"playNext($index)\"><source src=\"$url\" type=\"audio/mpeg\"></audio>";
                 // 在这里添加调用播放音频的代码，例如使用 HTML5 的 <audio> 标签播放音频
                 // 注意：如果音频文件过大，可能需要使用分段播放或者流式传输等技术，以避免占用过多内存或加载时间过长
             }
