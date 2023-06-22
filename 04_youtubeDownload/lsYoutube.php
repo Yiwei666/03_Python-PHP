54af94649b188639caf0fe28bf76b80ab780a616
aweill <84896083+aweill@users.noreply.github.com>
2023-06-22 23:30:27 +0800

    Update lsYoutube.php
    显示文件创建时间，大小，每列对齐

--- 04_youtubeDownload/lsYoutube.php
+++ 04_youtubeDownload/lsYoutube.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>文件列表</title>
+    <style>
+        pre {
+            font-family: monospace;
+        }
+    </style>
 </head>
 <body>
     <pre>
@@ -15,10 +20,31 @@
             if ($handle = opendir($directory)) {
                 // 读取目录中的文件和子目录到数组中
                 $files = [];
+                $maxFileNameLength = 0;
                 while (($file = readdir($handle)) !== false) {
                     // 排除当前目录（.）和上级目录（..）
                     if ($file != "." && $file != "..") {
-                        $files[] = $file;
+                        // 获取文件的创建日期和大小
+                        $filePath = $directory . '/' . $file;
+                        $creationDate = date("Y-m-d H:i:s", filectime($filePath));
+                        $fileSize = filesize($filePath);
+
+                        // 转换文件大小的单位
+                        $sizeUnit = 'B';
+                        if ($fileSize >= 1024 * 1024) {
+                            $fileSize = round($fileSize / (1024 * 1024), 2);
+                            $sizeUnit = 'MB';
+                        } elseif ($fileSize >= 1024) {
+                            $fileSize = round($fileSize / 1024, 2);
+                            $sizeUnit = 'KB';
+                        }
+
+                        // 将文件名、创建日期和文件大小拼接为一行
+                        $line = sprintf("%-{$maxFileNameLength}s  %s  %6s %s", $file, $creationDate, $fileSize, $sizeUnit);
+                        $files[] = $line;
+
+                        // 更新最大文件名长度
+                        $maxFileNameLength = max($maxFileNameLength, strlen($file));
                     }
                 }
                 // 关闭目录句柄
@@ -30,9 +56,9 @@
                 // 插入空行
                 echo "<br>";
 
-                // 打印排序后的文件名
-                foreach ($files as $file) {
-                    echo trim($file) . "<br>";
+                // 打印排序后的文件信息
+                foreach ($files as $line) {
+                    echo trim($line) . "<br>";
                 }
             }
         } else {
