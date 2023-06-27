1612e77cb595f1e4ff7534742310cf2f828345be
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-06-27 16:29:42 +0800

    Create 01_ablesci_crontab.py

---
+++ 03_smallTools/01_科研通签到/01_ablesci_crontab.py
@@ -0,0 +1,17 @@
+import datetime
+import time
+
+target_time = datetime.datetime.now().replace(hour=7, minute=0, second=0) + datetime.timedelta(minutes=60)
+end_time = datetime.datetime.now().replace(hour=8, minute=0, second=0)
+
+while datetime.datetime.now() < end_time:
+    current_time = datetime.datetime.now()
+
+    if current_time >= target_time:
+        import subprocess
+        command = '/home/00_software/01_Anaconda/bin/python /home/01_html/01_ablesci_spyder.py'
+        subprocess.call(command, shell=True)
+        break
+
+    time.sleep(50)
+
