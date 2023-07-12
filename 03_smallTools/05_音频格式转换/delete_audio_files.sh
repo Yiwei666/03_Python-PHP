8b125ba920e7207796b6afaeb73436f9463cb4e6
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-07-12 10:54:22 +0800

    Create delete_audio_files.sh

---
+++ 03_smallTools/05_音频格式转换/delete_audio_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 遍历当前目录下的所有 .m4a 和 .flac 文件
+for file in ./*.m4a ./*.flac; do
+    # 检查文件是否存在
+    if [ -e "$file" ]; then
+        # 删除文件
+        rm "$file"
+
+        # 检查是否删除成功
+        if [ $? -eq 0 ]; then
+            echo "已删除 $file"
+        else
+            echo "删除 $file 失败"
+        fi
+    fi
+done
