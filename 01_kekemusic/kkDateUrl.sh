f924bf485495182182dea8ce5fa3333329241471
aweill <84896083+aweill@users.noreply.github.com>
2023-08-31 09:09:03 +0800

    Update kkDateUrl.sh

--- 01_kekemusic/kkDateUrl.sh
+++ 01_kekemusic/kkDateUrl.sh
@@ -8,11 +8,10 @@ DB_PASS="<password>"
 TABLE_NAME="<table_name>"
 
 # Read and process the text file
-while IFS= read -r line; do
-  datetime=$(echo "$line" | cut -d ' ' -f 1,2)
-  url=$(echo "$line" | cut -d ',' -f 2)
+while IFS=',' read -r datetime url; do
   query="INSERT INTO $TABLE_NAME (datetime, url) VALUES ('$datetime', '$url');"
 
   # Execute the SQL query
   mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "$query"
 done < input.txt
+
