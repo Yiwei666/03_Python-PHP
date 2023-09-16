5412f0e2569f87c8b8ca1e3bd913042600b8bbe9
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-09-16 16:30:37 +0800

    Create ytbAudio_dwn_name.py

---
+++ 03_smallTools/09_YoutubeAudio/ytbAudio_dwn_name.py
@@ -0,0 +1,48 @@
+import socks
+import socket
+from pytube import YouTube
+import os
+import re
+
+# 设置SOCKS5代理
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 1080)
+socket.socket = socks.socksocket
+
+def clean_filename(filename):
+    # 使用正则表达式替换特殊字符为下划线
+    return re.sub(r'[\/:*?"<>|]', '_', filename)
+
+def download_highest_quality_audio(url):
+    try:
+        # 创建YouTube对象
+        yt = YouTube(url)
+        
+        # 获取所有音频流
+        audio_streams = yt.streams.filter(only_audio=True).order_by('abr').desc()
+        
+        # 选择最高比特率的音频流（通常是第一个）
+        highest_quality_audio = audio_streams[0]
+        
+        # 清理文件名，去除特殊字符
+        output_filename = clean_filename(yt.title) + '.mp3'
+        
+        # 下载音频流并保存为MP3，使用清理后的文件名
+        output_path = '.'
+        highest_quality_audio.download(output_path=output_path, filename=output_filename)
+        
+        # 获取下载文件的大小（以字节为单位）
+        file_size_bytes = os.path.getsize(os.path.join(output_path, output_filename))
+        
+        # 转换文件大小为MB
+        file_size_mb = file_size_bytes / (1024 * 1024)
+        
+        print(f"最高质量音频下载完成！保存为 '{output_filename}'，大小：{file_size_mb:.2f} MB")
+
+    except Exception as e:
+        print("下载失败:", str(e))
+
+# 提示用户输入要下载的YouTube视频的URL
+video_url = input("请输入要下载的YouTube视频链接：")
+
+# 调用下载最高质量音频函数
+download_highest_quality_audio(video_url)
