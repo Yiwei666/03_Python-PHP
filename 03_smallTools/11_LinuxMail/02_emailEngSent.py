d45ebba2b9f692a0a88446f81b8f42ddd7596929
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-12-16 21:02:29 +0800

    Update 02_emailEngSent.py
    添加多个收件者

--- 03_smallTools/11_LinuxMail/02_emailEngSent.py
+++ 03_smallTools/11_LinuxMail/02_emailEngSent.py
@@ -21,7 +21,7 @@ def get_unsent_pdf_filenames(pdf_folder, sent_pdf_list_path):
 
     return unsent_pdf_list
 
-def send_email(sender_email, sender_password, receiver_email, subject, body, attachment_path, sent_pdf_list_path):
+def send_email(sender_email, sender_password, receiver_emails, subject, body, pdf_folder, sent_pdf_list_path):
     # 设置邮件服务器和端口
     smtp_server = 'smtp.mail.yahoo.com'
     smtp_port = 587
@@ -34,15 +34,6 @@ def send_email(sender_email, sender_password, receiver_email, subject, body, att
         # 登录到邮箱
         server.login(sender_email, sender_password)
 
-        # 构建邮件
-        message = MIMEMultipart()
-        message['From'] = sender_email
-        message['To'] = receiver_email
-        message['Subject'] = subject
-
-        # 添加正文
-        message.attach(MIMEText(body, 'plain'))
-
         # 获取未发送的pdf文件名列表
         unsent_pdf_list = get_unsent_pdf_filenames(pdf_folder, sent_pdf_list_path)
 
@@ -54,15 +45,25 @@ def send_email(sender_email, sender_password, receiver_email, subject, body, att
         selected_pdf = random.choice(unsent_pdf_list)
         attachment_path = os.path.join(pdf_folder, selected_pdf)
 
-        # 添加附件
-        with open(attachment_path, 'rb') as file:
-            attachment = MIMEApplication(file.read(), Name=os.path.basename(attachment_path))
-            attachment['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
-            message.attach(attachment)
+        for receiver_email in receiver_emails:
+            # 构建邮件
+            message = MIMEMultipart()
+            message['From'] = sender_email
+            message['To'] = receiver_email
+            message['Subject'] = subject
 
-        # 发送邮件
-        server.sendmail(sender_email, receiver_email, message.as_string())
-        print(f"邮件发送成功，附件: {selected_pdf}")
+            # 添加正文
+            message.attach(MIMEText(body, 'plain'))
+
+            # 添加附件
+            with open(attachment_path, 'rb') as file:
+                attachment = MIMEApplication(file.read(), Name=os.path.basename(attachment_path))
+                attachment['Content-Disposition'] = f'attachment; filename="{os.path.basename(attachment_path)}"'
+                message.attach(attachment)
+
+            # 发送邮件
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            print(f"邮件发送成功，附件: {selected_pdf}")
 
         # 记录已发送的pdf文件名
         with open(sent_pdf_list_path, 'a') as file:
@@ -79,7 +80,7 @@ if __name__ == "__main__":
     # 设置发件人、收件人和附件路径
     sender_email = 'sender@yahoo.com'
     sender_password = 'aaaabbbbccccdddd'             # 16位应用程序专用密码
-    receiver_email = 'receiver@gmail.com'            # 收件者
+    receiver_emails = ['receiver1@gmail.com', 'receiver2@gmail.com']  # 收件者列表
     subject = 'Test Email with Attachment'
     body = 'This is a test email with attachment.'
     
@@ -88,4 +89,4 @@ if __name__ == "__main__":
     sent_pdf_list_path = '/home/01_html/02_emailPDF.txt'
 
     # 发送邮件
-    send_email(sender_email, sender_password, receiver_email, subject, body, None, sent_pdf_list_path)
+    send_email(sender_email, sender_password, receiver_emails, subject, body, pdf_folder, sent_pdf_list_path)
