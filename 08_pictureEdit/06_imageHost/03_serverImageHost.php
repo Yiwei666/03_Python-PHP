62ce75c1cb2fc38a5b3dda2052a493f7fd490dd0
aweill <84896083+aweill@users.noreply.github.com>
2023-12-22 09:44:24 +0800

    Update 03_serverImageHost.php

--- 08_pictureEdit/06_imageHost/03_serverImageHost.php
+++ 08_pictureEdit/06_imageHost/03_serverImageHost.php
@@ -1,23 +1,35 @@
 <?php
-// 设置上传目录
 $uploadDirectory = '/home/01_html/02_LAS1109/35_imageHost/';
 
-// 获取上传的文件
 $uploadedFile = $_FILES['image'];
 
-// 检查文件是否上传成功
 if ($uploadedFile['error'] !== UPLOAD_ERR_OK) {
     die('Upload failed with error code ' . $uploadedFile['error']);
 }
 
-// 构建目标文件路径，使用当前的年月日-时分秒时间戳作为文件名
 $timestamp = date('Ymd-His');
 $targetFileName = $timestamp . '.png';
 $targetFilePath = $uploadDirectory . $targetFileName;
 
-// 移动文件到目标路径
 if (move_uploaded_file($uploadedFile['tmp_name'], $targetFilePath)) {
-    echo 'File has been uploaded successfully.';
+
+    // 计算文件大小（KB和MB）
+    $fileSizeKB = round(filesize($targetFilePath) / 1024, 2); // 文件大小（KB）
+    $fileSizeMB = round(filesize($targetFilePath) / (1024 * 1024), 3); // 文件大小（MB）
+
+    // 替换路径中的 "/home/01_html" 为 "root"
+    $adjustedFilePath = str_replace('/home/01_html', 'http://120.46.81.41', $targetFilePath);
+
+    $response = [
+        'sizeKB' => $fileSizeKB,
+        'sizeMB' => $fileSizeMB,
+        'fileName' => $targetFileName,
+        'filePath' => $targetFilePath,
+        'adjustedPath' => $adjustedFilePath
+    ];
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
 } else {
     echo 'Upload failed.';
 }
