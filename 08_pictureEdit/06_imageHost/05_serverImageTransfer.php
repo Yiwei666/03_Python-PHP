468b1b26ecf57f535527cfaee2eb05ab516e7741
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2023-12-24 17:00:40 +0800

    Update 05_serverImageTransfer.php
    1. 显示正确的时间戳

--- 08_pictureEdit/06_imageHost/05_serverImageTransfer.php
+++ 08_pictureEdit/06_imageHost/05_serverImageTransfer.php
@@ -5,8 +5,12 @@ $imageName = isset($_POST['imageName']) ? $_POST['imageName'] : '';
 // 指定文本文件路径
 $filePath = '/home/01_html/05_imageTransferName.txt';
 
-// 获取当前时间（北京时间），使用 Y-m-d H:i:s 格式
-$currentTime = date('Y-m-d H:i:s', strtotime('+8 hours'));
+// 获取当前时间（GMT时间）
+$currentTimeGMT = gmdate('Y-m-d H:i:s');
+
+// 手动调整时区为北京时间
+date_default_timezone_set('Asia/Shanghai');
+$currentTime = date('Y-m-d H:i:s', strtotime($currentTimeGMT . '+8 hours'));
 
 // 将内容写入文本文件（以追加的方式）
 if (!empty($imageName)) {
