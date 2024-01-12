63cfb6405129f8503957ec33389d899514ea1cd7
aweill <84896083+aweill@users.noreply.github.com>
2024-01-12 19:14:59 +0800

    Add files via upload

---
+++ 01_GooglePodcast/04_VOALearningEnglish/01_VOA_execute_tasks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Step 1: 删除文件夹
+rm -r /home/01_html/09_VOALearningEnglish/01_audio
+
+# Step 2: 创建文件夹
+mkdir -p /home/01_html/09_VOALearningEnglish/01_audio
+
+# Step 3: 运行第一个脚本
+/usr/bin/bash /home/01_html/09_VOALearningEnglish/download_Random_mp3.sh
+
+# Step 4: 等待10分钟
+sleep 600  # 10分钟 = 10 * 60 秒
+
+# Step 5: 运行第二个脚本
+/usr/bin/bash /home/01_html/09_VOALearningEnglish/83_syn_azure2-1_to_AECS_VOA.sh
