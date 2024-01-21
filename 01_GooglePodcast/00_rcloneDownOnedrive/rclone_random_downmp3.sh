7bebb560bc30000dc5c8c8fc0b1822ff3cdb4350
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2024-01-21 14:52:17 +0800

    Add files via upload

---
+++ 01_GooglePodcast/00_rcloneDownOnedrive/rclone_random_downmp3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 远程路径
+remote_path="AECS-1109:AECS-1109/37_Economist/01_audio"
+
+# 本地路径
+local_path="/home/01_html/37_Economist/01_audio"
+
+# 随机选取的文件数量
+random_files_count=10
+
+# 1. 删除目录 $local_path
+rm -rf "$local_path"
+
+# 2. 创建目录 $local_path
+mkdir -p "$local_path"
+
+# 3. 使用rclone读取远程位置 $remote_path 下的所有文件名到一个数组中
+file_list=($(rclone ls "$remote_path" | awk '{print $NF}' | shuf))
+
+# 4. 从上述数组中随机选取 $random_files_count 个后缀名为mp3的文件名，下载到指定目录 $local_path
+count=0
+
+for file in "${file_list[@]}"; do
+    if [[ $file == *.mp3 ]]; then
+        if [ $count -lt $random_files_count ]; then
+            rclone copy "$remote_path/$file" "$local_path"
+            ((count++))
+        else
+            break
+        fi
+    fi
+done
+
+echo "任务完成"
