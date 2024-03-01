822258097f9134d316f04237cb45a4e793137ee1
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2024-03-01 19:58:26 +0800

    Add files via upload

---
+++ 01_GooglePodcast/15_podcastTemplate/rclone_limitFileSize.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 指定目录路径，判断该目录是否小于指定大小
+directory="/home/01_html/51_SEND7/01_audio"
+
+# 判断目录大小是否小于12GB
+size=$(du -s "$directory" | awk '{print $1}')
+limit=8000000  # 8GB的大小限制，阈值通常需要小于可用内存的一半
+
+if [ $size -lt $limit ]; then
+  echo "目录大小小于12GB，退出脚本"
+  exit 0
+fi
+
+# 目录大于等于15GB的情况
+echo "目录大小大于等于12GB，执行操作"
+
+# 删除文件 /home/01_html/51_SEND7/source.txt
+rm -f "/home/01_html/51_SEND7/source.txt"  && sleep 3
+
+# 运行脚本 /usr/bin/bash /home/01_html/51_SEND7/source.sh
+/usr/bin/bash "/home/01_html/51_SEND7/source.sh"
+
+# 等待30秒
+sleep 30
+
+# 删除目录 /home/01_html/51_SEND7/02_audio
+rm -rf "/home/01_html/51_SEND7/02_audio"  && sleep 3
+
+# 运行脚本 /usr/bin/bash /home/01_html/51_SEND7/source_move_to_target.sh
+/usr/bin/bash "/home/01_html/51_SEND7/source_move_to_target.sh"  && sleep 3
+
+# 执行 rclone 命令，onedrive上该目录需要提前创建，等待时间需要保证rclone上传完毕，同时新下载的文件大小小于阈值
+# 
+/usr/bin/rclone copy "/home/01_html/51_SEND7/02_audio" "cc1-1:cc1-1/51_SEND7/01_audio"  && sleep 1400
+
+# 删除目录，释放硬盘空间 /home/01_html/51_SEND7/02_audio
+rm -rf "/home/01_html/51_SEND7/02_audio"
+
---
+++ 01_GooglePodcast/15_podcastTemplate/source.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 获取脚本所在目录
+# script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_dir="/home/01_html/51_SEND7"
+
+# 指定目录
+directory="/home/01_html/51_SEND7/01_audio"
+
+# 检查目录是否存在
+if [ ! -d "$directory" ]; then
+  echo "指定的目录不存在"
+  exit 1
+fi
+
+# 切换到目录
+cd "$directory" || exit 1
+
+# 获取所有文件名并写入source.txt
+ls -1 > "$script_dir/source.txt"
+
+echo "文件名已写入到 source.txt 中"
