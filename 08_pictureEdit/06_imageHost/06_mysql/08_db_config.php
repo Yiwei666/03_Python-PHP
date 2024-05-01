0f8112fea7e0c6d6e57769ba6712f751303c72a2
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2024-05-01 15:31:32 +0800

    Update 08_db_config.php

--- 08_pictureEdit/06_imageHost/06_mysql/08_db_config.php
+++ 08_pictureEdit/06_imageHost/06_mysql/08_db_config.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost'; // 通常是 'localhost' 或一个IP地址
 $username = 'root'; // 数据库用户名
-$password = 'syw523,,,'; // 数据库密码
+$password = '123456789'; // 数据库密码
 $dbname = 'image_db'; // 数据库名称
 
 // 创建数据库连接
