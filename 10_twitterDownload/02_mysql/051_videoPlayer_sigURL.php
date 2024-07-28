a453aa3db4fac1717fe74559f37e3df66b99f5ff
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2024-07-28 13:26:50 +0800

    Update 051_videoPlayer_sigURL.php
    1. 检测设备终端类型，对桌面和移动设备设置不同的显示方式（长和宽不同）

--- 10_twitterDownload/02_mysql/051_videoPlayer_sigURL.php
+++ 10_twitterDownload/02_mysql/051_videoPlayer_sigURL.php
@@ -32,22 +32,16 @@ if (isset($_GET['logout'])) {
     exit;
 }
 
-
 // 生成视频URL签名
-
 function generateSignedUrl($videoName, $key, $expiryTime) {
     $signature = hash_hmac('sha256', $videoName . $expiryTime, $key);
-    // 调试输出生成的签名
-    // echo "PHP generated signature: " . $signature . "<br>";
-    // echo "PHP videoName: " . $videoName . "<br>";
-    // echo "PHP Expiry Time: " . $expiryTime . "<br>";
     return "https://mcha.me/05_twitter_video/{$videoName}?expires={$expiryTime}&signature={$signature}";
 }
 
 $videoName = urldecode($_GET['video']);
 $videoName = basename($videoName, ".mp4");
 
-$signingKey = 'your-signing-key-2';       // 签名密钥，确保与Node.js中的一致
+$signingKey = 'your-signing-key-2';
 $expiryTime = time() + 600;
 
 $videoUrl = generateSignedUrl($videoName . ".mp4", $signingKey, $expiryTime);
@@ -64,15 +58,33 @@ $videoUrl = generateSignedUrl($videoName . ".mp4", $signingKey, $expiryTime);
             align-items: center;
             height: 100vh;
             margin: 0;
+            background-color: #000; /* 背景色设置为黑色 */
+        }
+        .video-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 100%;
         }
         video {
-            width: auto;
-            height: 100%;
+            width: 100%;
+            height: auto;
+            max-width: 30vw; /* 设置为屏幕宽度的25% */
+            max-height: 60vh; /* 设置为屏幕高度的50% */
+        }
+        @media only screen and (max-width: 1024px) {
+            video {
+                max-width: 100%;
+                max-height: 70%;
+            }
         }
     </style>
 </head>
 <body>
-    <video id="videoPlayer" controls></video>
+    <div class="video-container">
+        <video id="videoPlayer" controls></video>
+    </div>
     <script>
         // 定义视频的路径
         const videoUrl = '<?php echo $videoUrl; ?>';
@@ -87,14 +99,30 @@ $videoUrl = generateSignedUrl($videoName . ".mp4", $signingKey, $expiryTime);
         videoPlayer.appendChild(sourceElement);
 
         videoPlayer.addEventListener('loadedmetadata', function() {
-            if (videoPlayer.videoWidth > videoPlayer.videoHeight) {
-                videoPlayer.style.width = '100%';
-                videoPlayer.style.height = 'auto';
-            } else {
-                videoPlayer.style.width = 'auto';
-                videoPlayer.style.height = '100%';
-            }
+            adjustVideoSize();
         });
+
+        function adjustVideoSize() {
+            const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+            const aspectRatio = videoPlayer.videoWidth / videoPlayer.videoHeight;
+            if (isMobileDevice) {
+                if (aspectRatio < 1) { // 高大于宽
+                    videoPlayer.style.width = '100%';
+                    videoPlayer.style.height = 'auto';
+                } else {
+                    videoPlayer.style.width = 'auto';
+                    videoPlayer.style.height = '70%';
+                }
+            } else {
+                if (aspectRatio < 1) { // 高大于宽
+                    videoPlayer.style.width = 'auto';
+                    videoPlayer.style.height = '60vh';
+                } else {
+                    videoPlayer.style.width = '30vw';
+                    videoPlayer.style.height = 'auto';
+                }
+            }
+        }
 
         /*
         // 页面加载时，从 localStorage 获取保存的播放时间并设置
