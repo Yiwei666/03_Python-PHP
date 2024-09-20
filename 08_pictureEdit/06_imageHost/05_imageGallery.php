adcaefe27ad670b323aa5c9e79d323c82f4c66f8
Yiwei666 <69843201+Yiwei666@users.noreply.github.com>
2024-09-20 21:14:28 +0800

    Update 05_imageGallery.php
    用户点击图片的 "Transfer" 按钮时，前端会弹出提示框要求用户输入密码。服务器端会核验密码，从而确定后续操作。

--- 08_pictureEdit/06_imageHost/05_imageGallery.php
+++ 08_pictureEdit/06_imageHost/05_imageGallery.php
@@ -160,23 +160,49 @@ echo '</div>';
 ?>
 
 <script>
-    var serverScriptUrl = '<?php echo $serverScript; ?>';
+    var serverScriptUrl = '<?php echo $serverScript; ?>'; // 服务器端处理脚本的URL
 
     function transferImage(imageUrl) {
+        // 提示用户输入密码
+        var userPassword = prompt('Please enter the password to transfer the image:');
+
+        // 如果用户取消输入，则退出
+        if (!userPassword) {
+            alert('Password input canceled.');
+            return;
+        }
+
+        // 获取图片的文件名
         var imageName = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+
+        // 创建 XMLHttpRequest 对象
         var xhr = new XMLHttpRequest();
         xhr.open('POST', serverScriptUrl, true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        // 设置回调函数，处理服务器端响应
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
-                    alert('Image transfer information recorded successfully!');
+                    if (xhr.responseText === 'success') {
+                        alert('Image transfer information recorded successfully!');
+                    }
+                } else if (xhr.status == 403) { // 处理密码错误的情况
+                    if (xhr.responseText.includes('error: incorrect password')) {
+                        alert('Incorrect password. Please try again.');
+                    }
+                } else if (xhr.status == 400) { // 处理 imageName 参数缺失的情况
+                    if (xhr.responseText.includes('error: imageName parameter is missing')) {
+                        alert('Error: image name is missing.');
+                    }
                 } else {
                     alert('Error: Unable to record transfer information.');
                 }
             }
         };
-        xhr.send('imageName=' + encodeURIComponent(imageName));
+
+        // 发送图片名称和用户输入的密码到服务器端进行验证
+        xhr.send('imageName=' + encodeURIComponent(imageName) + '&password=' + encodeURIComponent(userPassword));
     }
 </script>
 
